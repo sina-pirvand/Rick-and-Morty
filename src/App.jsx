65dd4b5eea8a1d7385635090c65a3ffd74f06bab
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-02-19 10:56:43 +0330

    fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
-import { allCharacters } from "../data/data";
+import Loading from "./components/Loading";
 import "./App.css";
-import { useState } from "react";
 
 const App = () => {
-  const [characters, setcharacters] = useState(allCharacters);
-  return (
+  const [characters, setcharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      setcharacters(data.results);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1500);
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="app">
       <Navbar numOfResult={characters.length} />
       <div className="main">
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1 className="loading-text">Loading ...</h1>
+    </div>
+  );
+};
+
+export default Loading;

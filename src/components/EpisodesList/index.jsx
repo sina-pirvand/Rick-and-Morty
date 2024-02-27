9fa577bbe92b8cf128803b4242b2faa57ce66769
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-02-27 10:25:26 +0330

    sort episodes feature added

--- src/components/EpisodesList/index.jsx
+++ src/components/EpisodesList/index.jsx
@@ -7,30 +7,48 @@ import {
 
 const EpisodesList = ({ episodes }) => {
   const [showAll, setShowAll] = useState(false);
+  const [sortBy, setSortBy] = useState(false);
   const toggleShowAll = () => {
     setShowAll(!showAll);
   };
+  let sortedEpisodes;
+  sortBy
+    ? (sortedEpisodes = [...episodes].sort(
+        (a, b) => new Date(a.created) - new Date(b.created)
+      ))
+    : (sortedEpisodes = [...episodes].sort(
+        (a, b) => new Date(b.created) - new Date(a.created)
+      ));
   return (
     <div className="character-episodes">
       <div className="character-episodes">
         <div className="title">
           <h2>List of Episodes:</h2>
-          <button>
-            <ArrowUpCircleIcon className="icon" />
+          <button
+            onClick={() => {
+              setSortBy((sortBy) => !sortBy);
+            }}
+          >
+            <ArrowUpCircleIcon
+              className="icon"
+              style={{ rotate: sortBy ? "0deg" : "180deg" }}
+            />
           </button>
         </div>
         <ul>
-          {episodes.slice(0, showAll ? episodes.length : 4).map((item, i) => (
-            <li key={item.id}>
-              <div>
-                {String(i + 1).padStart(2, "0")}: {item.episode}:{item.name}
-                <strong>{item.name}</strong>
-              </div>
-              <div className="badge badge--secondary">{item.air_date}</div>
-            </li>
-          ))}
+          {sortedEpisodes
+            .slice(0, showAll ? sortedEpisodes.length : 4)
+            .map((item, i) => (
+              <li key={item.id}>
+                <div>
+                  {String(i + 1).padStart(2, "0")}: {item.episode}:{item.name}
+                  <strong>{item.name}</strong>
+                </div>
+                <div className="badge badge--secondary">{item.air_date}</div>
+              </li>
+            ))}
         </ul>
-        {episodes.length > 4 && (
+        {sortedEpisodes.length > 4 && (
           <button
             className="btn btn--primary show-all-btn"
             onClick={toggleShowAll}

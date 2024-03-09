99a6124c857e6973f3387061e5d0caf4288dd75c
sina-prvnd <96621439+sina-prvnd@users.noreply.github.com>
2024-03-09 19:17:50 +0330

    useLocalStorage custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 import Navbar from "./components/Navbar";
 import CharacterList from "./components/CharacterList";
 import CharacterDetail from "./components/CharacterDetail";
 import Loading from "./components/Loading";
 import "./App.css";
-import useCharacters from "./hooks/useCharacters";
 
 const App = () => {
+  const [characters, setcharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [selectedId, setSelectedId] = useState(1);
   // const [favorite, setFavorite] = useState([]);
@@ -15,11 +17,7 @@ const App = () => {
     () => JSON.parse(localStorage.getItem("favorites")) || []
   );
 
-  const { isLoading, characters } = useCharacters(
-    "https://rickandmortyapi.com/api/character/?name",
-    search
-  );
-
+  const error = (err) => toast.error(err, { className: "toast" });
   const handleSelectedCharacter = (id) => {
     setSelectedId(id);
   };
@@ -32,6 +30,35 @@ const App = () => {
   const handleRemoveFavorite = (id) => {
     setFavorite((prev) => prev.filter((fav) => fav.id !== id));
   };
+
+  useEffect(() => {
+    const controler = new AbortController();
+    const signal = controler.signal;
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character/?name=${search}`,
+          { signal }
+        );
+        setcharacters(data.results);
+      } catch (err) {
+        // setcharacters([]);
+        if (!axios.isCancel())
+          error(`${err.response.data.error}. Most relevant results are shown`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (search.length > 0 && search.length < 3) {
+      toast.error("Atleast 3 letters needed to Search");
+      return;
+    }
+    fetchData();
+    return () => {
+      controler.abort();
+    };
+  }, [search]);
 
   useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favorite));
